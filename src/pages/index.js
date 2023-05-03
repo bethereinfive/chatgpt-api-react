aa78529fc2f3648683a5c19d5f7cfad9eff8f9c6
meloinu <meloinuofficial>
2023-05-03 01:44:25 -0600

    gpt

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     setChatLog((prevChatLog) => [...prevChatLog, { type: 'user', message: inputValue }])
 
     sendMessage(inputValue);
-    
+
     setInputValue('');
   }
 
@@ -45,8 +45,8 @@ export default function Home() {
 
   return (
     <div className="container mx-auto max-w-[700px]">
-      <div className="flex flex-col h-screen bg-gray-900">
-        <h1 className="bg-gradient-to-r from-blue-500 to-purple-500 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">ChatGPT</h1>
+      <div className="flex flex-col h-screen bg-black">
+        <h1 className="bg-green-400 text-transparent bg-clip-text text-center py-3 font-bold text-6xl">MAI</h1>
         <div className="flex-grow p-6">
           <div className="flex flex-col space-y-4">
           {
@@ -55,7 +55,7 @@ export default function Home() {
             message.type === 'user' ? 'justify-end' : 'justify-start'
             }`}>
             <div className={`${
-              message.type === 'user' ? 'bg-purple-500' : 'bg-gray-800'
+              message.type === 'user' ? 'bg-green-400' : 'bg-gray-800'
             } rounded-lg p-4 text-white max-w-sm`}>
             {message.message}
             </div>
@@ -73,9 +73,9 @@ export default function Home() {
       </div>
         </div>
         <form onSubmit={handleSubmit} className="flex-none p-6">
-          <div className="flex rounded-lg border border-gray-700 bg-gray-800">  
+          <div className="flex rounded-lg border border-gray-700 bg-gray-800">
         <input type="text" className="flex-grow px-4 py-2 bg-transparent text-white focus:outline-none" placeholder="Type your message..." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
-            <button type="submit" className="bg-purple-500 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
+            <button type="submit" className="bg-green-400 rounded-lg px-4 py-2 text-white font-semibold focus:outline-none hover:bg-purple-600 transition-colors duration-300">Send</button>
             </div>
         </form>
         </div>
